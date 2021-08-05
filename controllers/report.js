8d87e2900fd9b5f3f955d4584505fbfac73df9bc
DESKTOP-RIGDD5R\minrn <minrnullpo@gmail.com>
2021-08-05 19:43:55 +0700

    create handling rout api for report done

--- controllers/report.js
+++ controllers/report.js
@@ -1,3 +1,19 @@
+const convertBulanToName = bulan => {
+  if (bulan === "1") return "Januari";
+  if (bulan === "2") return "Februari";
+  if (bulan === "3") return "Maret";
+  if (bulan === "4") return "April";
+  if (bulan === "5") return "Mei";
+  if (bulan === "6") return "Juni";
+  if (bulan === "7") return "Juli";
+  if (bulan === "8") return "Agustus";
+  if (bulan === "9") return "September";
+  if (bulan === "10") return "Oktober";
+  if (bulan === "11") return "November";
+  if (bulan === "12") return "Desember";
+  return "";
+};
+
 const getIncomeByYear = db => (req, res) => {
   db("pesanan")
     .sum("TotalHarga as TotalPendapatan")
@@ -17,10 +33,13 @@ const getIncomeByYear = db => (req, res) => {
             .where({ TahunDibuat: "2021" })
             .groupBy("BulanDibuat")
             .then(resDataChart => {
+              const resultConvertBulan = resDataChart.map(item => {
+                return { namaBulan: convertBulanToName(item.bulan), ...item };
+              });
               res.status(200).json({
                 ...resPesanan[0],
                 ...resTotalOrder[0],
-                dataChart: resDataChart,
+                dataChart: resultConvertBulan,
               });
             });
         })
